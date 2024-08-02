eba389322458cc37eabdbd88b5db84a634353969
Jani Heinikoski <jani.heinikoski@student.lut.fi>
2024-08-02 15:02:58 +0300

    updated onboarding functionality

--- lib/routers/onboarding_router.py
+++ lib/routers/onboarding_router.py
@@ -3,10 +3,22 @@ from aiogram import Router, html
 from aiogram.types import Message, BotCommand
 from aiogram.filters import CommandStart, Command
 
+from typing import List
+
+from .elden_router import get_commands as get_elden_router_commands
+
 _router: Router = Router(name="onboarding_router")
 
 # Commands
-_help_command = BotCommand(command="help", description="Desc")
+_help_command = BotCommand(
+    command="help", description="Lists all commands the bot understands.")
+
+
+def get_commands() -> List[BotCommand]:
+    """
+    Returns a list of all commands this router is capable of handling. Used by onboarding (this) router to provide help.
+    """
+    return [_help_command]
 
 
 @_router.message(CommandStart())
@@ -14,7 +26,17 @@ async def start_handler(message: Message) -> None:
     """
     Handles /start command
     """
-    await message.answer(f"Hello, {html.bold(message.from_user.full_name)}!")
+    await message.answer(f"Hello, {html.bold(message.from_user.full_name)}! See /{_help_command.command} for a listing of commands you can use!")
+
+
+def add_commands_to_answer(to: List[str], heading: str, commands: List[BotCommand]) -> None:
+    """
+    Helper function for constructing the answer message to /help command 
+    """
+    to.append(f"\n{html.bold(heading)}")
+    for c in commands:
+        to.append(f"\n/{c.command} - {c.description}")
+    to.append('\n')
 
 
 @_router.message(Command(_help_command, ignore_case=True))
@@ -22,7 +44,15 @@ async def help_handler(message: Message) -> None:
     """
     Handles /help command
     """
-    await message.answer(f"These are the commands this bot understands:")
+    answer = [html.underline(
+        html.bold("These are the commands this bot understands:\n"))]
+    # Onboarding (this) router
+    add_commands_to_answer(
+        answer, "Onboarding:", get_commands())
+    # Elden router
+    add_commands_to_answer(
+        answer, "Elden Ring related commands:", get_elden_router_commands())
+    await message.answer("".join(answer))
 
 
 def get_router() -> Router:
